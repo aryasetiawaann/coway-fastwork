04e52b47172bf84b951e017ca8bc2ddc345378b4
aryasetiawaann <arya.setiawan@student.umn.ac.id>
2023-12-04 07:24:49 +0700

    Menyelesaikan responsive handphone

--- alatpemurni/js/app.js
+++ alatpemurni/js/app.js
@@ -1,18 +1,18 @@
 function sendWhatsAppVoucher() {
-  const voucher = document.getElementById('pop-up');
-  const success = document.getElementById('pop-up-success');
-  const name = document.getElementById('nama').value;
-  const phone = document.getElementById('phone').value;
-  const email = document.getElementById('email').value;
+  const voucher = document.getElementById("pop-up");
+  const success = document.getElementById("pop-up-success");
+  const name = document.getElementById("nama").value;
+  const phone = document.getElementById("phone").value;
+  const email = document.getElementById("email").value;
 
   if (name && phone && email) {
-    var url_wa = 'https://web.whatsapp.com/send';
+    var url_wa = "https://web.whatsapp.com/send";
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-      url_wa = 'whatsapp://send';
+      url_wa = "whatsapp://send";
     }
 
     var cs_whatsapp = [
-      '6281387954772', //<- masukan nomor tanpa 0 / +62 & baris nomor terakhir tanpa koma
+      "6281387954772", //<- masukan nomor tanpa 0 / +62 & baris nomor terakhir tanpa koma
     ];
 
     var rkey = Math.floor(Math.random() * cs_whatsapp.length);
@@ -20,9 +20,9 @@ function sendWhatsAppVoucher() {
 
     var link =
       url_wa +
-      '?phone=' +
+      "?phone=" +
       random_cs +
-      '&text=' +
+      "&text=" +
       encodeURIComponent(`Halo, saya ${name}
 *WhatsApp :* ${phone}
 *Email :* ${email}`);
@@ -31,23 +31,27 @@ function sendWhatsAppVoucher() {
       h = 540,
       left = Number(screen.width / 2 - w / 2),
       tops = Number(screen.height / 2 - h / 2),
-      popupWindow = window.open(link, '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=1, copyhistory=no, width=' + w + ', height=' + h + ', top=' + tops + ', left=' + left);
+      popupWindow = window.open(
+        link,
+        "",
+        "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=1, copyhistory=no, width=" + w + ", height=" + h + ", top=" + tops + ", left=" + left
+      );
 
     popupWindow.focus();
 
-    voucher.style.display = 'none';
-    success.style.display = 'flex';
+    voucher.style.display = "none";
+    success.style.display = "flex";
   } else {
-    alert('Lengkapi Form Terlebih Dahulu');
+    alert("Lengkapi Form Terlebih Dahulu");
   }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  const voucherButton = document.getElementById('voucher');
+document.addEventListener("DOMContentLoaded", function () {
+  const voucherButton = document.getElementById("voucher");
 
-  const pop = document.getElementById('bg-pop-up');
+  const pop = document.getElementById("bg-pop-up");
 
-  voucherButton.addEventListener('click', function () {
+  voucherButton.addEventListener("click", function () {
     sendWhatsAppVoucher();
   });
 
@@ -55,11 +59,20 @@ document.addEventListener('DOMContentLoaded', function () {
   //   pop.style.display = 'flex';
   // }, 2000);
 
-  const beli = document.querySelectorAll('.beli');
-  
+  const beli = document.querySelectorAll(".beli");
+
   beli.forEach(function (el) {
-    el.addEventListener('click', function () {
-      pop.style.display = 'flex';
+    el.addEventListener("click", function () {
+      pop.style.display = "flex";
     });
   });
 });
+
+function openLayer() {
+  var layer = document.getElementById("layer");
+  if (layer.style.display === "block") {
+    layer.style.display = "none";
+  } else {
+    layer.style.display = "block";
+  }
+}
